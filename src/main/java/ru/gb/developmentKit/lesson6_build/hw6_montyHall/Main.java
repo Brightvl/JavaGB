d0ef6de92bd9cd63d54f2e44e52f679817f0caf3
Iaroslav S <ziro-x-100@mail.ru>
2024-06-06 13:39:31 +0300

    JDC HW6 -> In Progress

---
+++ src/main/java/ru/gb/developmentKit/lesson6_build/hw6_montyHall/Main.java
@@ -0,0 +1,9 @@
+package ru.gb.developmentKit.lesson6_build.hw6_montyHall;
+
+public class Main {
+    public static void main(String[] args) {
+        MontyHallGame montyHallGame = new MontyHallGame();
+        montyHallGame.simulateGames();
+        montyHallGame.getResults();
+    }
+}
