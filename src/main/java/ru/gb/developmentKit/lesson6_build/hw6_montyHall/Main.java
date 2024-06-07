d36efbe272c761c9d09191e11cd3ea6fd8096acb
Iaroslav S <ziro-x-100@mail.ru>
2024-06-07 09:27:25 +0300

    JDC HW6 -> Done

--- src/main/java/ru/gb/developmentKit/lesson6_build/hw6_montyHall/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.developmentKit.lesson6_build.hw6_montyHall;
-
-public class Main {
-    public static void main(String[] args) {
-        MontyHallGame montyHallGame = new MontyHallGame();
-        montyHallGame.simulateGames();
-        montyHallGame.getResults();
-    }
-}
--- src/main/java/ru/gb/developmentKit/lesson6_build/hw6_montyHall/MontyHallGame.java
+++ src/main/java/ru/gb/developmentKit/lesson6_build/hw6_montyHall/MontyHallGame.java
@@ -1,24 +1,40 @@
 package ru.gb.developmentKit.lesson6_build.hw6_montyHall;
 
-/*
-В качестве задачи предлагаю вам реализовать код для демонстрации
-парадокса Монти Холла
-и наглядно убедиться в верности парадокса (запустить игру в цикле на 1000 и вывести итоговый счет).
-Необходимо:
-Создать свой Java Maven или Gradle проект;
-Самостоятельно реализовать прикладную задачу;
-Сохранить результат в HashMap<шаг теста, результат>
-Вывести на экран статистику по победам и поражениям
- */
+import lombok.Getter;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+/**
+ * В качестве задачи предлагаю вам реализовать код для демонстрации парадокса
+ * Монти Холла и наглядно убедиться в верности парадокса
+ * (запустить игру в цикле на 1000 и вывести итоговый счет).
+ * Необходимо:
+ * Создать свой Java Maven или Gradle проект;
+ * Подключить зависимость lombok.
+ * Можно подумать о подключении какой-нибудь математической библиотеки для работы со статистикой
+ * Самостоятельно реализовать прикладную задачу;
+ * Сохранить результат в HashMap<шаг теста, результат>.
+ * Вывести на экран статистику по победам и поражениям
+ */
+class Main {
+    public static void main(String[] args) {
+        MontyHallGame montyHallGame = new MontyHallGame();
+        montyHallGame.simulateGames();
+        montyHallGame.printResults();
+    }
+}
+
+/**
+ * Парадокс Монти-Холла
+ */
 public class MontyHallGame {
+
     public static final int NUM_TESTS = 1000;
+    @Getter
     private Map<Integer, Boolean> results;
-    Random random;
+    private final Random random;
 
     public MontyHallGame() {
         this.results = new HashMap<>();
@@ -26,8 +42,7 @@ public class MontyHallGame {
     }
 
     /**
-     * Метод имитирует проведение каждой игры.
-     * В цикле он создает сценарии для каждой игры
+     * Метод имитирует проведение каждой игры. В цикле он создает сценарии для каждой игры.
      */
     public void simulateGames() {
         for (int i = 1; i <= NUM_TESTS; i++) {
@@ -40,15 +55,16 @@ public class MontyHallGame {
             // Игрок решает изменить выбор или оставить его
             boolean switchDoor = random.nextBoolean();
             // Выигрыш или проигрыш
-            boolean win = switchDoor ? (chosenDoor != doorOpenedByHost) : (chosenDoor == carBehind);
+            boolean win = switchDoor == (chosenDoor != carBehind);
             results.put(i, win);
         }
     }
 
     /**
-     * Метод для действий ведущего, который открывает дверь, исключая выбор игрока и дверь с автомобилем
+     * Метод для действий ведущего, который открывает дверь, исключая выбор игрока и дверь с автомобилем.
+     *
      * @param chosenDoor выбранная игроком дверь
-     * @param carBehind расположение автомобиля
+     * @param carBehind  расположение автомобиля
      * @return номер двери
      */
     private int getDoorOpenedByHost(int chosenDoor, int carBehind) {
@@ -60,10 +76,14 @@ public class MontyHallGame {
     }
 
     /**
-     * Возвращает результаты всех проведенных игр
-     * @return Map
+     * Метод для подсчета и вывода статистики по победам и поражениям.
      */
-    public Map<Integer, Boolean> getResults() {
-        return results;
+    public void printResults() {
+        long wins = results.values().stream().filter(win -> win).count();
+        long losses = NUM_TESTS - wins;
+        System.out.println("Из 1000 игр:");
+        System.out.println("Побед: " + wins);
+        System.out.println("Поражений: " + losses);
     }
 }
+
