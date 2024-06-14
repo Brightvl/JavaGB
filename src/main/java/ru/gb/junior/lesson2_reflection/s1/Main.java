176d7b60d767269aecfa6ab5965b16c82eddfc96
Iaroslav S <ziro-x-100@mail.ru>
2024-06-14 08:56:54 +0300

    JJ S2 -> Done

--- src/main/java/ru/gb/junior/lesson2_reflection/s1/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.junior.lesson2_reflection.s1;
-
-public class Main {
-
-
-    
-}
---
+++ src/main/java/ru/gb/junior/lesson2_reflection/s1/anno/lib/Gender.java
@@ -0,0 +1,7 @@
+package ru.gb.junior.lesson2_reflection.s1.anno.lib;
+
+public enum Gender {
+
+  MALE, FEMALE
+
+}
