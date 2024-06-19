5deb132895df10aa4e6ee6512d1158eecd39bcdf
Iaroslav S <ziro-x-100@mail.ru>
2024-06-19 12:03:12 +0300

    JJ S3 -> Done

---
+++ src/main/java/ru/gb/junior/lesson3_JDBC_serialization/l3/L3.java
@@ -0,0 +1,14 @@
+package ru.gb.junior.lesson3_JDBC_serialization.l3;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class L3 {
+    public static void main(String[] args) throws Exception {
+        String str = "HI Bitches";
+        FileOutputStream fos = new FileOutputStream("ser");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(str);
+        oos.close();
+    }
+}
