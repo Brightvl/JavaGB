8ff15c62da8313f0768333599fc12aa4995ff97b
Iaroslav S <ziro-x-100@mail.ru>
2024-06-27 12:26:08 +0300

    JJ HW3 -> done

--- src/main/java/ru/gb/junior/lesson3_JDBC_Optional_serialization/hw3/Homework.java
+++ src/main/java/ru/gb/junior/lesson3_JDBC_Optional_serialization/hw3/Homework.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -45,10 +47,15 @@ public class Homework {
             // 4. Написать метод, который загружает Имя department по Идентификатору person
             System.out.println(getPersonDepartmentName(connection, 3));
 
-            // * Написать метод, который загружает Map<String, String>, в которой маппинг person.name ->
+            // 5.* Написать метод, который загружает Map<String, String>, в которой маппинг person.name ->
             //    department.name Пример: [{"person #1", "department #1"}, {"person #2", "department #3}]
+            System.out.println(getPersonDepartments(connection));
 
-            System.out.println();
+            // 6. ** Написать метод,    который загружает
+            //    Map<String, List<String>>, в которой маппинг department.name -> <person.name>
+            //    Пример: [{"department #1", ["person #1", "person #2"]}, {"department #2", ["person #3", "person #4"]} ]
+            System.out.println(getDepartmentPersons(connection));
+
         } catch (SQLException e) {
             System.err.println("Во время подключения произошла ошибка: " + e.getMessage());
         }
@@ -62,13 +69,14 @@ public class Homework {
      * @throws SQLException ошибка подключения к SQL
      */
     private static void createDepartmentTable(Connection connection) throws SQLException {
+        String query = """
+                create table department (
+                    id bigint primary key,
+                    name varchar(128) not null
+                )
+                """;
         try (Statement statement = connection.createStatement()) {
-            statement.execute("""
-                    create table department (
-                        id bigint primary key,
-                        name varchar(128) not null
-                    )
-                    """);
+            statement.execute(query);
         } catch (SQLException e) {
             System.err.println("Во время создания таблицы department произошла ошибка: " + e.getMessage());
             throw e;
@@ -137,13 +145,19 @@ public class Homework {
 
     /**
      * Получить имя department по id person
+     *
+     * @param connection соединение
+     * @param personId   id пользователя
+     * @return строка
+     * @throws SQLException исключение
      */
     private static String getPersonDepartmentName(Connection connection, long personId) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("""
+        String query = """
                 SELECT d.name
                 FROM person p
                 JOIN Department d ON p.department_id = d.id
-                WHERE p.id = ?""")) {
+                WHERE p.id = ?""";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setLong(1, personId);
             try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
@@ -183,22 +197,225 @@ public class Homework {
     }
 
 
-
     /**
-     * Пункт 5
+     * Пункт 5 * Написать метод, который загружает Map<String, String>, в которой маппинг person.name -> department.name
+     * Пример: [{"person #1", "department #1"}, {"person #2", "department #3}]
      */
     private static Map<String, String> getPersonDepartments(Connection connection) throws SQLException {
-        // FIXME: Ваш код тут
-        throw new UnsupportedOperationException();
+        HashMap<String, String> map = new HashMap<>();
+
+        String query = """
+                SELECT p.name AS person_name, d.name AS department_name
+                FROM person p
+                JOIN department d ON p.department_id = d.id
+                """;
+
+        try (PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            // если связанны join можно достать параметры из двух таблиц
+            while (resultSet.next()) {
+                String personName = resultSet.getString("person_name");
+                String departmentName = resultSet.getString("department_name");
+                map.put(personName, departmentName);
+            }
+
+            return map;
+        }
     }
 
     /**
-     * Пункт 6
+     * Пункт 6 ** Написать метод,который загружает Map<String, List<String>>, в которой маппинг department.name ->
+     * <person.name> Пример: [{"department #1", ["person #1", "person #2"]}, {"department #2", ["person #3", "person
+     * #4"]} ]
      */
     private static Map<String, List<String>> getDepartmentPersons(Connection connection) throws SQLException {
-        // FIXME: Ваш код тут
-        throw new UnsupportedOperationException();
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        String query = """
+                SELECT p.name AS person_name, d.name AS department_name
+                FROM person p
+                JOIN department d ON p.department_id = d.id
+                """;
+
+        try (PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                String personName = resultSet.getString("person_name");
+                String departmentName = resultSet.getString("department_name");
+
+                // Проверяем, есть ли уже список сотрудников для этого отдела
+                if (!map.containsKey(departmentName)) {
+                    // Если нет, создаем новый список
+                    map.put(departmentName, new ArrayList<>());
+                }
+                // Добавляем имя сотрудника в список для соответствующего отдела
+                map.get(departmentName).add(personName);
+            }
+
+            return map;
+        }
     }
+
+
+    //region 7 задание
+
+    /**
+     * Получить имя department по id person
+     *
+     * @param connection соединение
+     * @param personId   id пользователя
+     * @return строка
+     * @throws SQLException исключение
+     */
+    private static Department getPersonDepartment(Connection connection, long personId) throws SQLException {
+        String query = """
+                SELECT d.id, d.name
+                FROM person p
+                JOIN department d ON p.department_id = d.id
+                WHERE p.id = ?
+                """;
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setLong(1, personId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return new Department(resultSet.getLong("id"), resultSet.getString("name"));
+                } else {
+                    return null;
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @param connection
+     * @return
+     * @throws SQLException
+     */
+    private static Map<Person, Department> getPersonDepartmentMap(Connection connection) throws SQLException {
+        Map<Person, Department> map = new HashMap<>();
+
+        String query = """
+                SELECT
+                    p.id AS person_id,
+                    p.name AS person_name,
+                    p.age AS person_age,
+                    p.department_id,
+                    d.id AS department_id,
+                    d.name AS department_name
+                FROM person p
+                JOIN department d ON p.department_id = d.id
+                """;
+
+        try (PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                Person person = new Person(
+                        resultSet.getLong("person_id"),
+                        resultSet.getString("person_name"),
+                        resultSet.getInt("person_age"),
+                        resultSet.getLong("department_id")
+                );
+                Department department = new Department(
+                        resultSet.getLong("department_id"),
+                        resultSet.getString("department_name")
+                );
+                map.put(person, department);
+            }
+
+            return map;
+        }
+    }
+
+    /**
+     *
+     * @param connection
+     * @return
+     * @throws SQLException
+     */
+    private static Map<Department, List<Person>> getDepartmentPersonMap(Connection connection) throws SQLException {
+        Map<Department, List<Person>> map = new HashMap<>();
+
+        String query = """
+                SELECT p.id AS person_id, p.name AS person_name, p.age AS person_age, p.department_id, d.id AS department_id, d.name AS department_name
+                FROM person p
+                JOIN department d ON p.department_id = d.id
+                """;
+
+        try (PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                Department department = new Department(
+                        resultSet.getLong("department_id"),
+                        resultSet.getString("department_name")
+                );
+                Person person = new Person(
+                        resultSet.getLong("person_id"),
+                        resultSet.getString("person_name"),
+                        resultSet.getInt("person_age"),
+                        resultSet.getLong("department_id")
+                );
+
+                // Проверяем, есть ли уже список сотрудников для этого отдела
+                if (!map.containsKey(department)) {
+                    // Если нет, создаем новый список
+                    map.put(department, new ArrayList<>());
+                }
+                // Добавляем сотрудника в список для соответствующего отдела
+                map.get(department).add(person);
+            }
+
+            return map;
+        }
+    }
+
+    //endregion
 
 }
 
+@Getter
+@Setter
+class Person {
+    private long id;
+    private String name;
+    private int age;
+    private long departmentId;
+
+    public Person(long id, String name, int age, long departmentId) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", departmentId=" + departmentId +
+                '}';
+    }
+}
+
+@Getter
+@Setter
+class Department {
+    private long id;
+    private String name;
+
+    public Department(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
